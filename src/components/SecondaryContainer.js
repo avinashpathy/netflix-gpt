24ded89e2474be188dc8368098b24762b1fc5363
avinashpathy <avinashpathy2001@gmail.com>
2025-03-07 00:34:44 +0530

    added code for popular movies

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,12 +5,12 @@ import MovieList from "./MovieList";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    movies.nowPlayingMovies && (
+    movies && (
       <div className="bg-black">
         <div className="-mt-52 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList
             title={"Upcoming Movies"}
             movies={movies.nowPlayingMovies}
